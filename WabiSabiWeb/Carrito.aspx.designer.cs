8d9d375d8fcfdb463ccb32394cb8e3cb0ad8f3e9
pepezen <74072481+pepezen@users.noreply.github.com>
2021-06-04 17:16:43 +0200

    Arreglos en develop

--- WabiSabiWeb/Carrito.aspx.designer.cs
+++ WabiSabiWeb/Carrito.aspx.designer.cs
@@ -15,22 +15,22 @@ namespace WabiSabiWeb
     {
 
         /// <summary>
-        /// Control Button2.
+        /// Control Label2.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Label Label2;
 
         /// <summary>
-        /// Control Button3.
+        /// Control Label3.
         /// </summary>
         /// <remarks>
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button3;
+        protected global::System.Web.UI.WebControls.Label Label3;
 
         /// <summary>
         /// Control Button1.
@@ -40,5 +40,14 @@ namespace WabiSabiWeb
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
     }
 }
